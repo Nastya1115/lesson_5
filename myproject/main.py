0d4c7ef8767b71f6a6680b362cde3eca693e59f7
Nastya1115 <koshkahoroshaya@gmail.com>
2024-09-03 19:26:24 +0200

    message

--- myproject/main.py
+++ myproject/main.py
@@ -1,7 +1,30 @@
 import sys
 import os
 import django
-from myapp.models import *
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 django.setup()
+
+from myapp.models import *
+
+#math = Subject.objects.get(id=1)
+#teacher1 = Teacher(teacher_name='Николай', subject=math).save()
+go = True
+while go == True:
+    i = input('''
+        1. Add Subject \n
+        2. Add Teacher \n
+        3. Add Student \n
+        4. Add Student \n
+        5. Add Schedule \n
+        6. Add Grade \n
+        7. Exit  
+    ''')
+    if i == '1':
+        Subject.objects.create(subject_name=input("Enter subject's name: \n"), subject_description=input("Enter subject's description: \n"))
+    if i == '2':
+        Teacher.objects.create(teacher_name=input("Enter teacher's name: \n"), teacher_surname=input("Enter teacher's surname: \n"), subject=Subject.objects.get(subject_name=input("Enter teacher's subject: \n")))
+    elif i == '7':
+        go = False
+    else:
+        print('incorrect action!')
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,14 +3,29 @@ from django.db import models
 # Create your models here.
 class Subject(models.Model):
     subject_name = models.CharField(max_length=20)
+    subject_description = models.CharField(max_length=20)
 
 class Teacher(models.Model):
     teacher_name = models.CharField(max_length=20)
+    teacher_surname = models.CharField(max_length=20)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
 class Class(models.Model):
     class_name = models.CharField(max_length=20)
+    class_year = models.IntegerField()
 
 class Student(models.Model):
     student_name = models.CharField(max_length=20)
+    student_surname = models.CharField(max_length=20)
     student_class = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+class Schedule(models.Model):
+    schedule_name = models.CharField(max_length=10)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    shedule_class = models.ForeignKey(Class, on_delete=models.CASCADE)
+    Subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+class Grade(models.Model):
+    grade = models.IntegerField()
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
